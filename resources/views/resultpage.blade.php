5ed9c2a0d1e95dfd5361b195eef913fa41c31ca8
Raihan Nawawi <raihannawawi468@gmail.com>
2026-03-06 11:42:11 +0700

    Style: Menghapus semua class wrapper pada card kos, karena tidak memiliki pengaruh apapun terhadap desain card kos

--- resources/views/resultpage.blade.php
+++ resources/views/resultpage.blade.php
@@ -4,7 +4,7 @@
         <!-- Listings Header -->
         <div class="mb-4 flex items-center justify-between">
             <p class="text-sm md:text-base text-secondary">
-                10+ kos ditemukan
+                4 kos ditemukan
             </p>
 
             <button id="filtersBtn"
@@ -333,7 +333,7 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4 lg:gap-6">
 
             <!-- Kos Putri -->
-            <a wire:navigate href="/detailpage" class="group block rounded-lg focus:outline-none">
+            <a wire:navigate href="/detailpage">
                 {{-- Image --}}
                 <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
                     <img src="https://www.simplyhomy.com/wp-content/uploads/2017/01/bisnis-kos-kosan.jpg"
@@ -373,7 +373,7 @@
 
             </a>
             {{-- Kos Putra --}}
-            <a wire:navigate href="/detailpage" class="group block rounded-xl focus:outline-none">
+            <a wire:navigate href="/detailpage">
                 {{-- Image --}}
                 <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
                     <img src="https://thesmartlocal.com/indonesia/wp-content/uploads/2021/04/ventilation.jpeg"
@@ -413,7 +413,7 @@
 
             </a>
             {{-- Kos Campur --}}
-            <a wire:navigate href="/detailpage" class="group block rounded-xl focus:outline-none">
+            <a wire:navigate href="/detailpage">
                 {{-- Image --}}
                 <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
                     <img src="https://cdn1-production-images-kly.akamaized.net/fLCXhB2CU-knEOxENiP3Ge2Ie8M=/1280x720/smart/filters:quality(75):strip_icc()/kly-media-production/medias/861628/original/073424800_1429960385-3.JPG"
@@ -483,206 +483,6 @@
 
                     <p class="text-xs text-thirdary">
                         Kamar mandi dalam · Kasur · AC
-                    </p>
-
-                    <p class="text-sm">
-                        <span class="font-semibold underline">Rp462.683</span>
-                        <span class="text-thirdary"> / bulan</span>
-                    </p>
-                </div>
-
-            </a>
-            {{-- Kos Putra --}}
-            <a wire:navigate href="/detailpage" class="group block rounded-xl focus:outline-none">
-                {{-- Image --}}
-                <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
-                    <img src="https://thesmartlocal.com/indonesia/wp-content/uploads/2021/04/ventilation.jpeg"
-                        alt="Guesthouse"
-                        class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-
-                    <span
-                        class="absolute top-2 left-2 text-[11px] font-semibold bg-rose-500/90 text-white px-2 py-1 rounded-lg shadow">
-                        Sisa 2 kamar
-                    </span>
-                </div>
-                <!-- Content -->
-                <div class="mt-3 space-y-1">
-                    <div class="flex justify-between items-start gap-2 min-h-[28px] text-md font-semibold">
-                        <span>Kos Putra Siak</span>
-                        <span
-                            class="inline-flex items-center gap-1.5 text-xs font-medium bg-white text-secondary px-2.5 py-1 rounded-lg border border-gray-300">
-                            <i class="fa-solid fa-mars text-xs"></i>
-                            Putra
-                        </span>
-
-                    </div>
-
-                    <p class="text-sm text-secondary truncate">
-                        Kampung Dalam, Siak, Riau
-                    </p>
-
-                    <p class="text-xs text-thirdary">
-                        Kamar mandi dalam · Kasur
-                    </p>
-
-                    <p class="text-sm">
-                        <span class="font-semibold underline">Rp462.683</span>
-                        <span class="text-thirdary"> / bulan</span>
-                    </p>
-                </div>
-
-            </a>
-            {{-- Kos Campur --}}
-            <a wire:navigate href="/detailpage" class="group block rounded-xl focus:outline-none">
-                {{-- Image --}}
-                <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
-                    <img src="https://cdn1-production-images-kly.akamaized.net/fLCXhB2CU-knEOxENiP3Ge2Ie8M=/1280x720/smart/filters:quality(75):strip_icc()/kly-media-production/medias/861628/original/073424800_1429960385-3.JPG"
-                        alt="Guesthouse"
-                        class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-
-                    <span
-                        class="absolute top-2 left-2 text-[11px] font-semibold bg-rose-500/90 text-white px-2 py-1 rounded-lg shadow">
-                        Sisa 2 kamar
-                    </span>
-                </div>
-                <!-- Content -->
-                <div class="mt-3 space-y-1">
-                    <div class="flex justify-between items-start gap-2 min-h-[28px] text-md font-semibold">
-                        <span>Kos Campur</span>
-                        <span
-                            class="inline-flex items-center gap-1.5 text-xs font-medium bg-white text-secondary px-2.5 py-1 rounded-lg border border-gray-300">
-                            <i class="fa-solid fa-venus-mars text-xs"></i>
-                            Campuran
-                        </span>
-
-                    </div>
-
-                    <p class="text-sm text-secondary truncate">
-                        Kampung Dalam, Siak, Riau
-                    </p>
-
-                    <p class="text-xs text-thirdary">
-                        Kamar mandi dalam · Kasur
-                    </p>
-
-                    <p class="text-sm">
-                        <span class="font-semibold underline">Rp462.683</span>
-                        <span class="text-thirdary"> / bulan</span>
-                    </p>
-                </div>
-
-            </a>
-            <!-- Kos Putri -->
-            <a wire:navigate href="/detailpage" class="group block rounded-lg focus:outline-none">
-                {{-- Image --}}
-                <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
-                    <img src="https://www.simplyhomy.com/wp-content/uploads/2017/01/bisnis-kos-kosan.jpg"
-                        alt="Guesthouse"
-                        class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-
-                    <span
-                        class="absolute top-2 left-2 text-[11px] font-semibold bg-rose-500/90 text-white px-2 py-1 rounded-lg shadow">
-                        Sisa 2 kamar
-                    </span>
-                </div>
-                <!-- Content -->
-                <div class="mt-3 space-y-1">
-                    <div class="flex justify-between items-start gap-2 min-h-[28px] text-md font-semibold">
-                        <span>Kos Putri Melati</span>
-                        <span
-                            class="inline-flex items-center gap-1.5 text-xs font-medium bg-white text-secondary px-2.5 py-1 rounded-lg border border-gray-300">
-                            <i class="fa-solid fa-venus text-xs"></i>
-                            Putri
-                        </span>
-
-                    </div>
-
-                    <p class="text-sm text-secondary truncate">
-                        Kampung Dalam, Siak, Riau
-                    </p>
-
-                    <p class="text-xs text-thirdary">
-                        Kamar mandi dalam · Kasur · AC
-                    </p>
-
-                    <p class="text-sm">
-                        <span class="font-semibold underline">Rp462.683</span>
-                        <span class="text-thirdary"> / bulan</span>
-                    </p>
-                </div>
-
-            </a>
-            {{-- Kos Putra --}}
-            <a wire:navigate href="/detailpage" class="group block rounded-xl focus:outline-none">
-                {{-- Image --}}
-                <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
-                    <img src="https://thesmartlocal.com/indonesia/wp-content/uploads/2021/04/ventilation.jpeg"
-                        alt="Guesthouse"
-                        class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-
-                    <span
-                        class="absolute top-2 left-2 text-[11px] font-semibold bg-rose-500/90 text-white px-2 py-1 rounded-lg shadow">
-                        Sisa 2 kamar
-                    </span>
-                </div>
-                <!-- Content -->
-                <div class="mt-3 space-y-1">
-                    <div class="flex justify-between items-start gap-2 min-h-[28px] text-md font-semibold">
-                        <span>Kos Putra Siak</span>
-                        <span
-                            class="inline-flex items-center gap-1.5 text-xs font-medium bg-white text-secondary px-2.5 py-1 rounded-lg border border-gray-300">
-                            <i class="fa-solid fa-mars text-xs"></i>
-                            Putra
-                        </span>
-
-                    </div>
-
-                    <p class="text-sm text-secondary truncate">
-                        Kampung Dalam, Siak, Riau
-                    </p>
-
-                    <p class="text-xs text-thirdary">
-                        Kamar mandi dalam · Kasur
-                    </p>
-
-                    <p class="text-sm">
-                        <span class="font-semibold underline">Rp462.683</span>
-                        <span class="text-thirdary"> / bulan</span>
-                    </p>
-                </div>
-
-            </a>
-            {{-- Kos Campur --}}
-            <a wire:navigate href="/detailpage" class="group block rounded-xl focus:outline-none">
-                {{-- Image --}}
-                <div class="relative aspect-[4/3] rounded-xl overflow-hidden bg-gray-200">
-                    <img src="https://cdn1-production-images-kly.akamaized.net/fLCXhB2CU-knEOxENiP3Ge2Ie8M=/1280x720/smart/filters:quality(75):strip_icc()/kly-media-production/medias/861628/original/073424800_1429960385-3.JPG"
-                        alt="Guesthouse"
-                        class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-
-                    <span
-                        class="absolute top-2 left-2 text-[11px] font-semibold bg-rose-500/90 text-white px-2 py-1 rounded-lg shadow">
-                        Sisa 2 kamar
-                    </span>
-                </div>
-                <!-- Content -->
-                <div class="mt-3 space-y-1">
-                    <div class="flex justify-between items-start gap-2 min-h-[28px] text-md font-semibold">
-                        <span>Kos Campur</span>
-                        <span
-                            class="inline-flex items-center gap-1.5 text-xs font-medium bg-white text-secondary px-2.5 py-1 rounded-lg border border-gray-300">
-                            <i class="fa-solid fa-venus-mars text-xs"></i>
-                            Campuran
-                        </span>
-
-                    </div>
-
-                    <p class="text-sm text-secondary truncate">
-                        Kampung Dalam, Siak, Riau
-                    </p>
-
-                    <p class="text-xs text-thirdary">
-                        Kamar mandi dalam · Kasur
                     </p>
 
                     <p class="text-sm">
