0d75f214ce9913c201913b6e68b4f56593234d83
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-03-31 09:46:04 +0700

    Refactor: Memisahkan styling class accordion dari app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,19 @@
 
     <title>{{ $title ?? config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    {{-- Global Font --}}
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
+    <style>
+        body {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
 </head>
 
 <body class="bg-base">
@@ -27,32 +34,5 @@
 
     @livewireScripts
 </body>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
-
-    body {
-        font-family: 'Inter', sans-serif;
-    }
-
-    .accordion-content {
-        max-height: 0;
-        overflow: hidden;
-        transition: max-height 0.3s ease-in-out;
-    }
-
-    .accordion-content.active {
-        max-height: 500px;
-    }
-
-    .accordion-chevron {
-        transition: transform 0.3s ease-in-out;
-    }
-
-    .accordion-item.active .accordion-chevron {
-        transform: rotate(180deg);
-    }
-</style>
-{{-- Accordion Section END --}}
 
 </html>
